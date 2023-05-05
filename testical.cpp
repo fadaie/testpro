e85dfff29f6824cecb1da0f13abf665a59167314
Ali Fadaie Tehrani <64353075+fadaie@users.noreply.github.com>
2023-05-06 01:42:46 +0330

    Added file

---
+++ testical.cpp
@@ -0,0 +1,16 @@
+#include<cstdio>
+ using namespace std;
+ int main(){
+   int s1, s2, s3, sum;
+   printf("Enter mark\n");
+   scanf("%d", &s1);
+   printf("Enter mark\n");
+   scanf("%d", &s2);
+   printf("Enter mark\n");
+   scanf("%d", &s3);
+   sum = s1 + s2 + s3;
+   printf("%d\n", sum);
+   return 0;
+ }
+ 
+ //test project.
